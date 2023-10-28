94aac6470a1d830d4458efb1ee8282f559f3b1d7
fukumamire <rinka_karyou@hotmail.com>
2023-10-28 20:37:15 +0900

    AuhorRequest.phpのauthorize、rulresの設定　エラーメッセージの表示　

--- src/app/Http/Requests/AuthorRequest.php
+++ src/app/Http/Requests/AuthorRequest.php
@@ -13,7 +13,7 @@ class AuthorRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class AuthorRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required',
+            'age' => 'integer|min:0|max:150',
+            'nationality' => 'required'  
         ];
     }
 }
